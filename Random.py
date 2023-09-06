4f7ae059dcbc2a404541244a93de4007e28feee4
Jáhìd hassan <140346514+SKBER-CYBER@users.noreply.github.com>
2023-09-06 15:55:31 +0600

    Update Random.py

--- Random.py
+++ Random.py
@@ -6,6 +6,6 @@ bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
     os.system('xdg-open https://facebook.com/groups/790381136153614/')
-    import V8
+    import go
 else:
     exit('\033[1;31m[×] Sorry Device Not Support')
