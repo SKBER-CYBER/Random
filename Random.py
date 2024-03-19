88c53ba13158971d99359d943a30b8e25bdc7089
LêgêñÐ ᎧᎶᎶᎩ <140346514+SKBER-CYBER@users.noreply.github.com>
2024-03-19 09:14:54 +0600

    Update Random.py

--- Random.py
+++ Random.py
@@ -5,7 +5,7 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
-    os.system('xdg-open https://facebook.com/groups/790381136153614/')
-    import Parvez
+    os.system('xdg-open https://www.facebook.com/oggytool')
+    import boka
 else:
     exit('\033[1;31m[×] Sorry Device Not Support')
